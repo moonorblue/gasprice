a984b204f36f3621cf83a2260b7908590ea55e0d
moonorblue <moonorblue@gmail.com>
2015-08-09 16:14:59 +0800

    Initial Commit

---
+++ GasPrice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GasPrice
+//
+//  Created by KaeJer Cho on 2015/8/9.
+//  Copyright (c) 2015年 KaeJer Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GasPrice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GasPrice
+//
+//  Created by KaeJer Cho on 2015/8/9.
+//  Copyright (c) 2015年 KaeJer Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
